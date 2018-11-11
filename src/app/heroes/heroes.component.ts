48bbc5c2377b486d0fa66745d7b1aa9f079142c8
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2018-11-11 18:16:44 +0000

    Allow a hero to be selected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes";
 
 @Component({
@@ -7,11 +8,16 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
